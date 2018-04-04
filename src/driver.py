534b5342a395d9e602cde39fabf993d6ddf3a0ba
Anthony Piddubny <anton.p@qualisystems.com>
2018-04-04 18:08:15 +0300

    update shell with new cloudshell-networking-devices and cloudshell-traffic-virtual packages

--- src/driver.py
+++ src/driver.py
@@ -1,33 +1,33 @@
-import copy
 import json
 import time
 from datetime import datetime
 from datetime import timedelta
 
-from cloudshell.api.cloudshell_api import SetConnectorRequest
 from cloudshell.core.context.error_handling_context import ErrorHandlingContext
 from cloudshell.devices.autoload.autoload_builder import AutoloadDetailsBuilder
 from cloudshell.devices.driver_helper import get_api
 from cloudshell.devices.driver_helper import get_cli
 from cloudshell.devices.driver_helper import get_logger_with_thread_id
+from cloudshell.devices.standards.traffic.virtual.chassis.configuration_attributes_structure import TrafficGeneratorVChassisResource
 from cloudshell.shell.core.driver_context import AutoLoadDetails
 from cloudshell.shell.core.resource_driver_interface import ResourceDriverInterface
+from cloudshell.traffic.virtual.resource_driver_interface import VirtualTrafficGeneratorResourceDriverInterface
+from cloudshell.traffic.virtual.runners.connect_child_resources import ConnectChildResourcesRunner
+
 from traffic.ixvm.vchassis.api.client import IxVMChassisHTTPClient
 from traffic.ixvm.vchassis.autoload import models
-from traffic.ixvm.vchassis.configuration_attributes_structure import TrafficGeneratorVChassisResource
 from traffic.ixvm.vchassis.runners.configuration_runner import IxVMConfigurationRunner
+
 
 SERVICE_STARTING_TIMEOUT = 60 * 60
 CHASSIS_STRUCTURE_APPEARANCE_TIMEOUT = 10 * 60
-ATTR_REQUESTED_SOURCE_VNIC = "Requested Source vNIC Name"
-ATTR_REQUESTED_TARGET_VNIC = "Requested Target vNIC Name"
 SHELL_TYPE = "CS_VirtualTrafficGeneratorChassis"
 SHELL_NAME = "IxVM Virtual Traffic Chassis 2G"
 MODULE_MODEL = "{}.VirtualTrafficGeneratorModule".format(SHELL_NAME)
 PORT_MODEL = "{}.VirtualTrafficGeneratorPort".format(SHELL_NAME)
 
 
-class IxVMVirtualChassisDriver(ResourceDriverInterface):
+class IxVMVirtualChassisDriver(ResourceDriverInterface, VirtualTrafficGeneratorResourceDriverInterface):
     def __init__(self):
         """Constructor must be without arguments, it is created with reflection at run time"""
         self._cli = None
@@ -129,10 +129,10 @@ class IxVMVirtualChassisDriver(ResourceDriverInterface):
             chassis_data = api_client.get_chassis()[0]
             chassis_id = chassis_data["id"]
 
-            chassis_res = models.Chassis(shell_type=SHELL_TYPE,
-                                         shell_name=resource_config.shell_name,
-                                         name="IxVm Virtual Chassis {}".format(chassis_id),
-                                         unique_id=chassis_id)
+            chassis_res = models.IxVMChassis(shell_type=SHELL_TYPE,
+                                             shell_name=resource_config.shell_name,
+                                             name="IxVm Virtual Chassis {}".format(chassis_id),
+                                             unique_id=chassis_id)
 
             ports_data = {}
             for port_data in api_client.get_ports():
@@ -147,9 +147,9 @@ class IxVMVirtualChassisDriver(ResourceDriverInterface):
                 module_id = module_data["id"]
                 module_number = module_data["cardNumber"]
 
-                module_res = models.Module(shell_name=resource_config.shell_name,
-                                           name="IxVm Virtual Module {}".format(module_number),
-                                           unique_id=module_id)
+                module_res = models.IxVMModule(shell_name=resource_config.shell_name,
+                                               name="IxVm Virtual Module {}".format(module_number),
+                                               unique_id=module_id)
 
                 logger.info("Adding Module {} to the Chassis".format(module_number))
                 chassis_res.add_sub_resource(module_number, module_res)
@@ -158,9 +158,9 @@ class IxVMVirtualChassisDriver(ResourceDriverInterface):
                     port_id = port_data["id"]
                     port_number = port_data["portNumber"]
 
-                    port_res = models.Port(shell_name=resource_config.shell_name,
-                                           name="Port {}".format(port_number),
-                                           unique_id=port_id)
+                    port_res = models.IxVMPort(shell_name=resource_config.shell_name,
+                                               name="Port {}".format(port_number),
+                                               unique_id=port_id)
 
                     logger.info("Adding Port {} under the module {}".format(port_number, module_id))
                     module_res.add_sub_resource(port_number, port_res)
@@ -176,7 +176,6 @@ class IxVMVirtualChassisDriver(ResourceDriverInterface):
 
     def connect_child_resources(self, context):
         """
-
         :type context: cloudshell.shell.core.driver_context.ResourceCommandContext
         :rtype: str
         """
@@ -184,126 +183,21 @@ class IxVMVirtualChassisDriver(ResourceDriverInterface):
         logger.info("Connect child resources command started")
 
         with ErrorHandlingContext(logger):
-            resource_config = TrafficGeneratorVChassisResource.from_context(context,
-                                                                            shell_type=SHELL_TYPE,
-                                                                            shell_name=SHELL_NAME)
-
-            if not resource_config.address or resource_config.address.upper() == "NA":
-                return
-
-            api = get_api(context)
-
             resource_name = context.resource.fullname
             reservation_id = context.reservation.reservation_id
             connectors = context.connectors
+            api = get_api(context)
 
-            if not context.connectors:
-                return "Success"
+            connect_operation = ConnectChildResourcesRunner(logger=logger,
+                                                            cs_api=api)
 
-            resource = api.GetResourceDetails(resource_name)
+            ports = connect_operation.get_ports(resource_name=resource_name,
+                                                port_model=PORT_MODEL)
 
-            to_disconnect = []
-            to_connect = []
-            temp_connectors = []
-            ports = self._get_ports(resource)
-
-            for connector in connectors:
-                me, other = self._set_remap_connector_details(connector, resource_name, temp_connectors)
-                to_disconnect.extend([me, other])
-
-            connectors = temp_connectors
-
-            # these are connectors from app to vlan where user marked to which interface the connector should be connected
-            connectors_with_predefined_target = [connector for connector in connectors if connector.vnic_id != ""]
-
-            # these are connectors from app to vlan where user left the target interface unspecified
-            connectors_without_target = [connector for connector in connectors if connector.vnic_id == ""]
-
-            for connector in connectors_with_predefined_target:
-                if connector.vnic_id not in ports.keys():
-                    raise Exception("Tried to connect an interface that is not on reservation - " + connector.vnic_id)
-
-                else:
-                    if hasattr(ports[connector.vnic_id], "allocated"):
-                        raise Exception(
-                            "Tried to connect several connections to same interface: " + ports[connector.vnic_id])
-
-                    else:
-                        to_connect.append(SetConnectorRequest(SourceResourceFullName=ports[connector.vnic_id].Name,
-                                                              TargetResourceFullName=connector.other,
-                                                              Direction=connector.direction,
-                                                              Alias=connector.alias))
-                        ports[connector.vnic_id].allocated = True
-
-            unallocated_ports = [port for key, port in ports.items() if not hasattr(port, "allocated")]
-
-            if len(unallocated_ports) < len(connectors_without_target):
-                raise Exception("There were more connections to TeraVM than available interfaces after deployment.")
-            else:
-                for port in unallocated_ports:
-                    if connectors_without_target:
-                        connector = connectors_without_target.pop()
-                        to_connect.append(SetConnectorRequest(SourceResourceFullName=port.Name,
-                                                              TargetResourceFullName=connector.other,
-                                                              Direction=connector.direction,
-                                                              Alias=connector.alias))
-
-            if connectors_without_target:
-                raise Exception("There were more connections to TeraVM than available interfaces after deployment.")
-
-            api.RemoveConnectorsFromReservation(reservation_id, to_disconnect)
-            api.SetConnectorsInReservation(reservation_id, to_connect)
-
-            return "Success"
-
-    @staticmethod
-    def _set_remap_connector_details(connector, resource_name, connectors):
-        attribs = connector.attributes
-        if resource_name in connector.source.split("/"):
-            remap_requests = attribs.get(ATTR_REQUESTED_SOURCE_VNIC, "").split(",")
-
-            me = connector.source
-            other = connector.target
-
-            for vnic_id in remap_requests:
-                new_con = copy.deepcopy(connector)
-                IxVMVirtualChassisDriver._update_connector(new_con, me, other, vnic_id)
-                connectors.append(new_con)
-
-        elif resource_name in connector.target.split("/"):
-            remap_requests = attribs.get(ATTR_REQUESTED_TARGET_VNIC, "").split(",")
-
-            me = connector.target
-            other = connector.source
-
-            for vnic_id in remap_requests:
-                new_con = copy.deepcopy(connector)
-                IxVMVirtualChassisDriver._update_connector(new_con, me, other, vnic_id)
-                connectors.append(new_con)
-        else:
-            raise Exception("Oops, a connector doesn't have required details:\n Connector source: {0}\n"
-                            "Connector target: {1}\nPlease contact your admin".format(connector.source,
-                                                                                      connector.target))
-
-        return me, other
-
-    @staticmethod
-    def _update_connector(connector, me, other, vnic_id):
-        connector.vnic_id = vnic_id
-        connector.me = me
-        connector.other = other
-
-    @staticmethod
-    def _get_ports(resource):
-        ports = {}
-        idx = 2
-        for module in (module for module in resource.ChildResources if module.ResourceModelName == MODULE_MODEL):
-            for port in (port for port in module.ChildResources if port.ResourceModelName == PORT_MODEL):
-                ports[str(idx)] = port
-                idx += 1
-
-        return ports
-
+            return connect_operation.connect_child_resources(connectors=connectors,
+                                                             ports=ports,
+                                                             resource_name=resource_name,
+                                                             reservation_id=reservation_id)
 
 if __name__ == "__main__":
     import mock
--- src/traffic/ixvm/vchassis/autoload/models.py
+++ src/traffic/ixvm/vchassis/autoload/models.py
@@ -1,81 +1,15 @@
-from cloudshell.devices.standards.base import AbstractResource
+from cloudshell.devices.standards.traffic.virtual.autoload_structure import Chassis
+from cloudshell.devices.standards.traffic.virtual.autoload_structure import Module
+from cloudshell.devices.standards.traffic.virtual.autoload_structure import Port
 
 
-AVAILABLE_SHELL_TYPES = ["CS_VirtualTrafficGeneratorChassis"]
-
-
-class Chassis(AbstractResource):
+class IxVMChassis(Chassis):
     RESOURCE_MODEL = "IxVM Virtual Chassis"
-    RELATIVE_PATH_TEMPLATE = "CH"
-
-    def __init__(self, shell_name, name, unique_id, shell_type):
-        super(Chassis, self).__init__(shell_name, name, unique_id)
-
-        if shell_name:
-            self.shell_name = "{}.".format(shell_name)
-            if shell_type in AVAILABLE_SHELL_TYPES:
-                self.shell_type = "{}.".format(shell_type)
-            else:
-                raise Exception(self.__class__.__name__, "Unavailable shell type {shell_type}."
-                                                         "Shell type should be one of: {avail}"
-                                .format(shell_type=shell_type, avail=", ".join(AVAILABLE_SHELL_TYPES)))
-        else:
-            self.shell_name = ""
-            self.shell_type = ""
 
 
-class Module(AbstractResource):
-    RESOURCE_MODEL = "Virtual Traffic Generator Module"
-    RELATIVE_PATH_TEMPLATE = "M"
-
-    @property
-    def device_model(self):
-        """
-
-        :return:
-        """
-        return self.attributes.get("{}Model".format(self.namespace), None)
-
-    @device_model.setter
-    def device_model(self, value):
-        """
-
-        :param value:
-        :return:
-        """
-        self.attributes["{}Model".format(self.namespace)] = value
+class IxVMModule(Module):
+    pass
 
 
-class Port(AbstractResource):
-    RESOURCE_MODEL = "Virtual Traffic Generator Port"
-    RELATIVE_PATH_TEMPLATE = "P"
-
-    @property
-    def logical_name(self):
-        return self.attributes.get("{}Logical Name".format(self.namespace), None)
-
-    @logical_name.setter
-    def logical_name(self, value):
-        """
-
-        :param value:
-        :return:
-        """
-        self.attributes["{}Logical Name".format(self.namespace)] = value
-
-    @property
-    def mac_address(self):
-        """
-
-        :return:
-        """
-        return self.attributes.get("{}MAC Address".format(self.namespace), None)
-
-    @mac_address.setter
-    def mac_address(self, value):
-        """
-
-        :param value:
-        :return:
-        """
-        self.attributes["{}MAC Address".format(self.namespace)] = value
+class IxVMPort(Port):
+    pass
